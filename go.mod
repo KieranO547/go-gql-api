f9101a6eb7e4df2077181a529ecf34fcbf4ac130
Kieran Osgood <kieran@analogrepublic.com>
2020-12-13 12:41:16 +0000

    feat: add docker-compose for postgressql and setup database connection

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.13
 
 require (
 	github.com/99designs/gqlgen v0.13.0
+	github.com/graphql-go/graphql v0.7.9 // indirect
+	github.com/lib/pq v1.9.0 // indirect
 	github.com/vektah/gqlparser/v2 v2.1.0
 )
--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -9,11 +11,22 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/kierano547/go-gql-api/graph"
 	"github.com/kierano547/go-gql-api/graph/generated"
+	_ "github.com/lib/pq"
 )
 
 const defaultPort = "8080"
 
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "postgres"
+	pass   = "changeme"
+	dbname = "postgres"
+)
+
 func main() {
+	initDatabase()
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
@@ -26,4 +39,24 @@ func main() {
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func initDatabase() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, pass, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
 }
